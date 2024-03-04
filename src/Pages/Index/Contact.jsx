509a4fafec00487f9395f1e00db5a727b2b3a83a
Abraham Solabi <67491400+blessme247@users.noreply.github.com>
2024-03-05 00:49:16 +0100

    Fix: Updated Twitter Username

--- src/Pages/Index/Contact.jsx
+++ src/Pages/Index/Contact.jsx
@@ -112,7 +112,7 @@ const Contact = () => {
             </Link>
             <Link
               className="socialLink"
-              to="https://twitter.com/blessme_xo"
+              to="https://twitter.com/lifeofblessme"
               target="_blank"
             >
               Twitter{" "}
