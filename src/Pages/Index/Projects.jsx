7e4890cbc2e752cc40ef54f906d2c36b326f0fcf
Abraham Solabi <67491400+blessme247@users.noreply.github.com>
2025-09-20 08:42:40 +0100

    Update Projects.jsx

--- src/Pages/Index/Projects.jsx
+++ src/Pages/Index/Projects.jsx
@@ -13,32 +13,15 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
 
-  return (
-//   <section className="projectsSection" >
-
-//     <div className="container">
-//     <div className="sectionHeading projectSectionHeading">Projects</div>
-//     </div>
-//     <div className="projects">
-//   <Project image={project3} projectTitle="Zinary Website" projectLink="https://www.zinary.com/"/>
-//   <Project image={project1} projectTitle="Automedsys Website" projectLink="https://automedsys-clone.vercel.app/"/>
-//   <div className="divider" />
-
-//   <Project image={project2} projectTitle="E-commerce Website" projectLink="https://audiophile-ecommerce-247.vercel.app/"/>
-//   <div className="divider" />
-
-//   <div className="divider" />
-// </div>
-
-// </section>
-
-    const projects = [
+     const projects = [
     {title: "HR Management dashboard", url: "https://humanline-dashboard-app.vercel.app/", year: "2025"},
     {title: "Karashi Jewelers Blog", url: "https://karashijewelers.com", year: "2025"},
     {title: "Zinary Website", url: "https://www.zinary.com", year: "2025"},
     {title: "Automedsys Website", url: "https://automedsys-clone.vercel.app/", year: "2025"},
     {title: "Audiophile E-commerce", url: "https://audiophile-ecommerce-247.vercel.app/", year: "2023"},
     ]
+
+  return (
 
  <section className='mobileProjectsSection '>
 
