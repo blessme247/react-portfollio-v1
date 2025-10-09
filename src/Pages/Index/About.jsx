502fbb4f2b62b27e074a65089b0c6e5bc800896d
Abraham Solabi <67491400+blessme247@users.noreply.github.com>
2025-10-09 08:09:56 +0100

    Update About.jsx

--- src/Pages/Index/About.jsx
+++ src/Pages/Index/About.jsx
@@ -7,11 +7,7 @@ const About = () => {
         <div className="sectionHeading text__reveal">About Me</div>
 
         <p className="aboutText" data-animation="paragraph">
-          I am a frontend developer with a specialization in improving web
-          performance and search engine optimization. With a keen eye for
-          detail and a passion for delivering exceptional user experiences, I
-          excel at creating fast and conversion-focused websites that rank
-          highly in search engine results.
+         Frontend Engineer with 3+ years of experience building scalable, high-performance web and mobile applications for startups and international teams. Proficient in React, Next.js, and React Native, with a strong background in accessibility, state management, API integrations, and performance optimization.
         </p>
       </div>
     </section>
