c9a4202026b230aee2b6beda9362a43a5c1c3d08
Abraham Solabi <67491400+blessme247@users.noreply.github.com>
2025-11-03 07:38:22 +0100

    Add Email Verify project link to Tools section

--- src/Pages/Index/Tools.jsx
+++ src/Pages/Index/Tools.jsx
@@ -16,8 +16,15 @@ const Tools = () => {
 
 <div className="container">
     <div className="sectionHeading projectSectionHeading text__reveal">Tools</div>
+  
+  
         <div className="projects">
-            <Link className="project" target="blank" to="https://svg-shape-to-path-converter.vercel.app/"> 
+            <Link className="project" target="blank" to="https://github.com/blessme247/email-verify/"> 
+            <p className='projectTitle'>Email Verify</p>
+            <p className="projectYear">2025</p>
+            </Link>
+
+           <Link className="project" target="blank" to="https://svg-shape-to-path-converter.vercel.app/"> 
             <p className='projectTitle'>Svg shape to path converter</p>
             <p className="projectYear">2025</p>
             </Link>
