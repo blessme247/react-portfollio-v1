acd7eadc6e572334c8bbb558b2f4c3a6725258c0
Abraham Solabi <67491400+blessme247@users.noreply.github.com>
2026-02-02 22:47:50 +0100

    Update About Me section with new experience details
    Updated experience to reflect 4+ years and added Node.js proficiency.

--- src/Pages/Index/About.jsx
+++ src/Pages/Index/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <div className="sectionHeading text__reveal">About Me</div>
 
         <p className="aboutText" data-animation="paragraph">
-         Frontend Engineer with 3+ years of experience building scalable, high-performance web and mobile applications for startups and international teams. Proficient in React, Next.js, and React Native, with a strong background in accessibility, state management, API integrations, and performance optimization.
+         Frontend Engineer with 4+ years of experience building scalable, high-performance web and mobile applications for startups and international teams. Proficient in React, Next.js, React Native and Node.js, with a strong background in accessibility, state management, API integrations, and performance optimization.
         </p>
       </div>
     </section>
